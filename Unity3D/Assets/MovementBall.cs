610dfabb33fd149670fe641b23673eb5d972df1d
Javier Iglesias <javiglesias18@gmail.com>
2015-02-13 16:31:47 +0100

    Primera prueba
    Esta es la primera prueba que ago con unity en 3D, es una bola que se
    mueve segun WASD

---
+++ Unity3D/Assets/MovementBall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementBall : MonoBehaviour 
+{
+
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		float translation = Input.GetAxis("Vertical")*10;
+		float rotation = Input.GetAxis("Horizontal")*10;
+		//translation *= Time.deltaTime;
+		Vector3 move = new Vector3(translation,0.0F,rotation);
+		rigidbody.AddForce(move);
+	}
+}
